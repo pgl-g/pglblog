1ccf3263ac6ae6ff7c958632696bc469bf2f1707
彭格列 <1801688332@qq.com>
2022-05-16 23:44:54 +0800

    fest: home style

--- components/IndexList/index.js
+++ components/IndexList/index.js
@@ -4,7 +4,6 @@ import IndexListStyle from '../../assets/css/components/indexList.module.css';
 
 
 const IndexList = () => {
-
   return (
     <div className={IndexListStyle.container}>
       <div className={IndexListStyle.listItem}>
@@ -57,6 +56,105 @@ const IndexList = () => {
         <hr />
       </div>
 
+      <div className={IndexListStyle.listItem}>
+        <div className={IndexListStyle.item_title}>
+          <div className={`${IndexListStyle.tit_t} ${IndexListStyle.border_r}`}>pgl</div>
+          <div className={`${IndexListStyle.tit} ${IndexListStyle.border_r}`}>3个月前</div>
+          <div className={`${IndexListStyle.tit}`}>个人分享</div>
+        </div>
+
+        <div className={IndexListStyle.flex_content}>
+          <div className={IndexListStyle.flex_1}>
+            <div className={IndexListStyle.title}>实现某种粉丝</div>
+            <div className={IndexListStyle.contents}>
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              </div>
+            <div className="show_num">
+              <EyeFilled />
+              18
+            </div>
+          </div>
+          <img src="https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png" alt="" />
+        </div>
+        <hr />
+      </div>
+      
+      <div className={IndexListStyle.listItem}>
+        <div className={IndexListStyle.item_title}>
+          <div className={`${IndexListStyle.tit_t} ${IndexListStyle.border_r}`}>pgl</div>
+          <div className={`${IndexListStyle.tit} ${IndexListStyle.border_r}`}>3个月前</div>
+          <div className={`${IndexListStyle.tit}`}>个人分享</div>
+        </div>
+
+        <div className={IndexListStyle.flex_content}>
+          <div className={IndexListStyle.flex_1}>
+            <div className={IndexListStyle.title}>实现某种粉丝</div>
+            <div className={IndexListStyle.contents}>
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              </div>
+            <div className="show_num">
+              <EyeFilled />
+              18
+            </div>
+          </div>
+          <img src="https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png" alt="" />
+        </div>
+        <hr />
+      </div>
+
+      <div className={IndexListStyle.listItem}>
+        <div className={IndexListStyle.item_title}>
+          <div className={`${IndexListStyle.tit_t} ${IndexListStyle.border_r}`}>pgl</div>
+          <div className={`${IndexListStyle.tit} ${IndexListStyle.border_r}`}>3个月前</div>
+          <div className={`${IndexListStyle.tit}`}>个人分享</div>
+        </div>
+
+        <div className={IndexListStyle.flex_content}>
+          <div className={IndexListStyle.flex_1}>
+            <div className={IndexListStyle.title}>实现某种粉丝</div>
+            <div className={IndexListStyle.contents}>
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              </div>
+            <div className="show_num">
+              <EyeFilled />
+              18
+            </div>
+          </div>
+          <img src="https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png" alt="" />
+        </div>
+        <hr />
+      </div>
+
+      <div className={IndexListStyle.listItem}>
+        <div className={IndexListStyle.item_title}>
+          <div className={`${IndexListStyle.tit_t} ${IndexListStyle.border_r}`}>pgl</div>
+          <div className={`${IndexListStyle.tit} ${IndexListStyle.border_r}`}>3个月前</div>
+          <div className={`${IndexListStyle.tit}`}>个人分享</div>
+        </div>
+
+        <div className={IndexListStyle.flex_content}>
+          <div className={IndexListStyle.flex_1}>
+            <div className={IndexListStyle.title}>实现某种粉丝</div>
+            <div className={IndexListStyle.contents}>
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。
+              </div>
+            <div className="show_num">
+              <EyeFilled />
+              18
+            </div>
+          </div>
+          <img src="https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png" alt="" />
+        </div>
+        <hr />
+      </div>
       <div className={IndexListStyle.listItem}>
         <div className={IndexListStyle.item_title}>
           <div className={`${IndexListStyle.tit_t} ${IndexListStyle.border_r}`}>pgl</div>
