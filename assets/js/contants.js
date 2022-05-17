d922057c148c8ecd3eec41db227ef96ca9ce0b7f
彭格列 <1801688332@qq.com>
2022-05-17 16:06:14 +0800

    feat: 优化部分数据

--- assets/js/contants.js
+++ assets/js/contants.js
@@ -5,7 +5,7 @@ const NAVS = [
     type: 'home'
   },
   {
-    name: '文章列表',
+    name: '作品集',
     link: '/articlelists',
     type: 'articlelists'
   },
@@ -16,6 +16,90 @@ const NAVS = [
   },
 ]
 
+const INDEXLIST = [
+  {
+    id: 1,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 2,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 3,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 4,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 5,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 6,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 1,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+  {
+    id: 7,
+    username: 'pgl',
+    starTime: '3月前',
+    activeType: '个人分享',
+    title: '实现某种功能',
+    content: '不要犯我曾经犯过的错误！ 我2015年刚刚开始学习前端开发的时候，我在文档和在线教程上了解到了许多技术，我浪费大量时间去学习这些技术。',
+    show: 18,
+    logo: 'https://image.immortalboy.cn/public/uploads/2022/02/13/1644752217552675.png'
+  },
+]
+
 export {
-  NAVS
+  NAVS,
+  INDEXLIST
 }
