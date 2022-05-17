606f9de996a47999a1f02ea1353f7d2e749d879e
彭格列 <1801688332@qq.com>
2022-05-17 15:22:23 +0800

    feat: 左边头部样式

---
+++ components/AvatarInfo/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { GithubFilled, WechatFilled, QqOutlined } from '@ant-design/icons';
+import StyleAvatar from '../../assets/css/components/avatarInfo.module.css';
+
+
+const AvatarInfo = () => {
+  return (
+    <div className={StyleAvatar.avatarinfo}>
+      
+      <div className={StyleAvatar.avatar_userInfo}>
+        <img src="https://images.wosaimg.com/e0/de8374888df2087861989b02571e0fd8f4258c.jpeg" alt="" />
+        <span className={StyleAvatar.userName}>对对对你说的都对</span>
+
+        <div className={StyleAvatar.avatar_description}>这是我的个人博客，记录了一些研发过程中遇到的坑。</div>
+      </div>
+
+      <div className={StyleAvatar.avatarStatus}>
+        <div className={StyleAvatar.avatarAccount}>
+            <GithubFilled className={StyleAvatar.avatarFlex} />
+            <WechatFilled className={StyleAvatar.avatarFlex} />
+            <QqOutlined className={StyleAvatar.avatarFlex} />
+        </div>
+      </div>
+
+    </div>  
+  )
+}
+
+export default AvatarInfo;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Col, Row } from 'antd';
 import Head from 'next/head';
 import Header from '../components/Header';
 import IndexList from '../components/IndexList';
+import AvatarInfo from '../components/AvatarInfo';
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         <Header />
         <Row justify="center" className="content">
           <Col className="content-left" xs={0} sm={0} md={8} lg={6} xl={4}>
-            asxasxa
+            <AvatarInfo />
           </Col>
           <Col className="content-right" xs={24} sm={24} md={12} lg={12} xl={12}>
             <IndexList />
