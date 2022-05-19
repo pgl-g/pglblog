9cf7bad4347cb755e9233f2724e372dc4abfef5e
彭格列 <1801688332@qq.com>
2022-05-19 20:13:23 +0800

    feat: 优化页面

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,16 +1,22 @@
 import React, { memo } from 'react';
 import { Row, Col } from 'antd';
+import { useRouter } from 'next/router';
 import Menu from '../Menus';
 import { NAVS } from '../../assets/js/contants';
 import styles from '../../assets/css/components/header.module.css';
 
 
 const Header = memo(() => {
+  const router = useRouter();
+  const handleToHome = () => {
+    router.replace('/')
+  }
+
   return (
     <div className={styles.header}>
       <Row justify="center" align="middle">
           <Col xs={24} sm={24} md={8} lg={6} xl={4}>
-            <div className={styles.header_left}>
+            <div className={styles.header_left} onClick={handleToHome}>
               <div>
                 <span className={styles.header_logo}>彭格列</span>
                 <span className={styles.header_text}>编程shy</span>
