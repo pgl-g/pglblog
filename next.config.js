a40e44ab5c63cc69a6f61e5335f3c8aeca50695d
彭格列 <1801688332@qq.com>
2022-05-21 18:24:53 +0800

    fix: chnage

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
-const withCss = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins');
+// let withCss = require('@zeit/next-css');
+// let withLess = require("@zeit/next-less");
 
 // TODO: 暂时不配置，有问题！！！
-if(typeof require !== 'undefined'){
-    require.extensions['.css']=file=>{}
-}
+// if(typeof require !== 'undefined'){
+//     require.extensions['.css']=file=>{}
+// }
 
-module.exports = {}
+
+
+// const plugins = withPlugins([], {
+//     webpack5: false
+// })
+
+// const stylesa = withCss(
+//     withLess({
+//         cssModules: true
+//     })
+// );
+
+module.exports = {
+    // stylesa,
+    // plugins
+}
