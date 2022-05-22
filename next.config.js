dd0b6cad7dbb1c4c31e6c3c064321fbf804ec708
彭格列 <1801688332@qq.com>
2022-05-22 14:56:32 +0800

    feat: next.config. js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,17 @@
-const withPlugins = require('next-compose-plugins');
-// let withCss = require('@zeit/next-css');
-// let withLess = require("@zeit/next-less");
+let withCss = require('@zeit/next-css');
 
 // TODO: 暂时不配置，有问题！！！
-// if(typeof require !== 'undefined'){
-//     require.extensions['.css']=file=>{}
-// }
+if(typeof require !== 'undefined'){
+    require.extensions['.css']=file=>{}
+}
 
 
 
-// const plugins = withPlugins([], {
-//     webpack5: false
-// })
 
-// const stylesa = withCss(
-//     withLess({
-//         cssModules: true
-//     })
-// );
+const stylesa = withCss(
+   {}
+);
 
 module.exports = {
-    // stylesa,
-    // plugins
+    stylesa,
 }
