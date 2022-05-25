4fb0ab59a4ee0caf4ead78b6ba5cfa877f9f6f18
彭格列 <1801688332@qq.com>
2022-05-25 18:01:58 +0800

    fix: 详情页

--- components/detaileList/index.js
+++ components/detaileList/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import DetaileListStyle from '../../assets/css/components/detaileList.module.css';
 
 const DetaileList = () => {
 
   return (
 
-    <div>
+    <div className={DetaileListStyle.container}>
       详情页
     </div>
   )
--- pages/indexDetaile/[id].js
+++ pages/indexDetaile/[id].js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Col, Row } from 'antd';
+import Head from 'next/head';
 
+import Header from '../../components/Header';
 import DetaileList from '../../components/detaileList';
 
-
+import IndexDetaileStyle from '../../assets/css/pages/indexDetaile.module.css';
 
 function IndexDetaile() {
   return (
-    <div>
-      <DetaileList />
+    <div className={IndexDetaileStyle.detaile_container}>
+      <Head>
+        <title>编程shy-个人博客</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Header />
+        <Row justify="center" className={IndexDetaileStyle.detaile_warpper}>
+          <Col className="warpper_right" xs={24} sm={24} md={14} lg={14} xl={14}>
+            <DetaileList />
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
